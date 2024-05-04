dddb49271be6cf52237425a1172950a18c400052
Ting-Hsuan Chang <65819949+tinghsuan-chang@users.noreply.github.com>
2024-05-03 20:56:39 -0400

    Update cf.R

--- code/cf.R
+++ code/cf.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(grf)
 
+# data prep -----------------------------------------------------------------------------------
 dat <- readRDS("processed_data/dat.RDS")
 df <- dat %>% 
   mutate(fire_type_bin = case_when(
@@ -20,31 +21,69 @@ df <- df[complete.cases(df),] %>%
   # 759 complete cases
 df <- df[df$daily_pop_smokePM != Inf,] # remove 4 obs where daily_pop_smokePM = Inf -> 755 obs
 
-Y1 <- df$total_pop_smokePM # outcome 1: total smoke severity
-Y2 <- df$daily_pop_smokePM # outcome 2: daily smoke severity
-W <- df$fire_type_bin # treatment: fire type
-B <- as.data.frame(df$burn_severity_mode) # burn severity
-X <- df[,9:24] # confounders
-V <- df[,c(8:24)] # burn severity + confounders
-state <- df$state_num
 
-set.seed(905)
-Y <- Y2
-# estimate expected outcome model m(x) = E[Y|B,X]
-Y.forest <- regression_forest(V, Y)
-Y.hat <- predict(Y.forest)$predictions
+# causal forest -------------------------------------------------------------------------
+cf_func <- function(df, outcome, clus = "Y") {
+  if (outcome == "total") {
+    Y <- df$total_pop_smokePM # outcome 1: total smoke severity
+  } else if (outcome == "daily") {
+    Y <- df$daily_pop_smokePM # outcome 2: daily smoke severity
+  }
+  W <- df$fire_type_bin # treatment: fire type
+  B <- as.data.frame(df$burn_severity_mode) # burn severity
+  X <- df[,9:24] # confounders
+  V <- df[,c(8:24)] # burn severity + confounders
+  state <- df$state_num
+  
+  # estimate expected outcome model m(x) = E[Y|B,X]
+  if (clus == "Y") {
+    Y.forest <- regression_forest(V, Y, clusters = state)
+  } else {
+    Y.forest <- regression_forest(V, Y)
+  }
+  Y.hat <- predict(Y.forest)$predictions
+  
+  # estimate propensity score model e(x) = E[W|X]
+  if (clus == "Y") {
+    W.forest <- regression_forest(X, W, clusters = state)
+  } else {
+    W.forest <- regression_forest(X, W)
+  }
+  W.hat <- predict(W.forest)$predictions
+  
+  # causal forest 
+  if (clus == "Y") {
+    cf <- causal_forest(V, Y, W, Y.hat = Y.hat, W.hat = W.hat, clusters = state)
+  } else {
+    cf <- causal_forest(V, Y, W, Y.hat = Y.hat, W.hat = W.hat)
+  }
+  varimp <- variable_importance(cf)  
+  selected <- which(varimp > mean(varimp)) 
+  att <- average_treatment_effect(cf, target.sample = "overlap") 
+  
+  return(list(cf = cf, 
+              var = colnames(V[,selected]), 
+              var_imp = varimp[selected], 
+              att = att))
+}
 
-# estimate propensity score model e(x) = E[W|X]
-W.forest <- regression_forest(X, W)
-W.hat <- predict(W.forest)$predictions
+set.seed(1); cf_func(df, outcome = "total")
+set.seed(1); cf_func(df, outcome = "daily")
 
-# causal forest
-cf_all <- causal_forest(V, Y, W, Y.hat = Y.hat, W.hat = W.hat)
-varimp <- variable_importance(cf_all) 
-selected <- which(varimp > mean(varimp)) 
+# plot TOC ------------------------------------------------------------------------------
+# CA 
+df_CA <- df %>% filter(state == "CA") # 400 obs
+set.seed(1); cf_CA <- cf_func(df_CA, outcome = "total", clus = "N")
+rate <- rank_average_treatment_effect(cf_CA$cf, 
+                                      df_CA$forest, 
+                                      subset = !(cf_CA$cf$W.hat %in% c(0, 1)))
+plot(rate)
 
-cf <- causal_forest(V[,selected], Y, W, Y.hat = Y.hat, W.hat = W.hat)
-test_calibration(cf) 
-att <- average_treatment_effect(cf, target.sample = "overlap") 
-
+# FL + GA
+df_east <- df %>% filter(state %in% c("FL","GA")) # 355 obs
+set.seed(1); cf_east <- cf_func(df_east, outcome = "total", clus = "N")
+rate <- rank_average_treatment_effect(cf_east$cf, 
+                                      df_east$forest, 
+                                      subset = !(cf_east$cf$W.hat %in% c(0, 1)))
+plot(rate)
 
